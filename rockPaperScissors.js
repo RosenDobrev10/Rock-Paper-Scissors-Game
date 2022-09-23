7f076bd86903b6d6b23d393dedde6c023bc80e50
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-09-23 12:07:22 +0300

    Add some features

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,3 +1,5 @@
+const colors = require('colors/safe');
+
 function rockpaperScissors(playerTurn) {
 
     const rock = 'rock'
@@ -5,43 +7,53 @@ function rockpaperScissors(playerTurn) {
     const scissors = 'scissors'
     let computerTurn;
 
+    let wins = 0
+    let losses = 0
 
-    if (playerTurn === 'r' || playerTurn === 'rock') {
-        playerTurn = rock
-    } else if (playerTurn === 'p' || playerTurn === 'paper') {
-        playerTurn = paper
-    } else if (playerTurn === 's' || playerTurn === 'scissors') {
-        playerTurn = scissors
-    } else {
-        console.log('Invalid input. Try again...')
-        return
+    while (wins <= 2 && losses <= 2) {
+
+        if (playerTurn === 'r' || playerTurn === 'rock') {
+            playerTurn = rock
+        } else if (playerTurn === 'p' || playerTurn === 'paper') {
+            playerTurn = paper
+        } else if (playerTurn === 's' || playerTurn === 'scissors') {
+            playerTurn = scissors
+        } else {
+            console.log(colors.black('Invalid input. Try again...'))
+            return
+        }
+        console.log(`You choose ${playerTurn}`)
+
+        let computerRandomNumber = Math.floor(Math.random() * 3) + 1
+
+        if (computerRandomNumber === 1) {
+            computerTurn = rock
+        } else if (computerRandomNumber === 2) {
+            computerTurn = paper
+        } else if (computerRandomNumber === 3) {
+            computerTurn = scissors
+        }
+        console.log(`The computer choses ${computerTurn}`)
+
+        if ((playerTurn === 'rock' && computerTurn === 'rock') ||
+            (playerTurn === 'paper' && computerTurn === 'paper') ||
+            (playerTurn === 'scissors' && computerTurn === 'scissors')) {
+            console.log(colors.yellow('This game was a draw!'))
+        } else if ((playerTurn === 'rock' && computerTurn === 'scissors') ||
+            (playerTurn === 'paper' && computerTurn === 'rock') ||
+            (playerTurn === 'scissors' && computerTurn === 'paper')) {
+            console.log(colors.green('You win!'))
+            wins++
+        } else if ((playerTurn === 'rock' && computerTurn === 'paper') ||
+            (playerTurn === 'paper' && computerTurn === 'scissors') ||
+            (playerTurn === 'scissors' && computerTurn === 'rock')) {
+            console.log(colors.red('You lose!'))
+            losses++
+        }
+        console.log(`The temporary result is ${wins} : ${losses}`)
     }
-    console.log(`You choose ${playerTurn}`)
-
-    let computerRandomNumber = Math.floor(Math.random() * 3) + 1
-
-    if (computerRandomNumber === 1) {
-        computerTurn = rock
-    } else if (computerRandomNumber === 2) {
-        computerTurn = paper
-    } else if (computerRandomNumber === 3) {
-        computerTurn = scissors
-    }
-    console.log(`The computer choses ${computerTurn}`)
-
-    if ((playerTurn === 'rock' && computerTurn === 'rock') ||
-        (playerTurn === 'paper' && computerTurn === 'paper') ||
-        (playerTurn === 'scissors' && computerTurn === 'scissors')) {
-        console.log('this game was a draw!')
-    } else if ((playerTurn === 'rock' && computerTurn === 'scissors') ||
-        (playerTurn === 'paper' && computerTurn === 'rock') ||
-        (playerTurn === 'scissors' && computerTurn === 'paper')) {
-        console.log('You win!')
-    } else if ((playerTurn === 'rock' && computerTurn === 'paper') ||
-        (playerTurn === 'paper' && computerTurn === 'scissors') ||
-        (playerTurn === 'scissors' && computerTurn === 'rock')) {
-        console.log('You lose!')
-    }
-
+    console.log(`The final result is ${wins} : ${losses}`)
+    wins > losses ? console.log(`You have WON the game!!!`) : console.log(`You have LOST the game !!!`)
 }
-rockpaperScissors('w')
+const choices = ['r', 'p', 's', 'rock', 'paper', 'scissors']
+rockpaperScissors(choices[Math.floor(Math.random() * 6)])

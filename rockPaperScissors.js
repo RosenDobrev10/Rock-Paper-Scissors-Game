906f8b4459d4956e1f4c09d1360de23eff497719
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-09-23 11:24:00 +0300

    Update rockPaperScissors.js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,42 +1,47 @@
-const rock = 'rock'
-const paper = 'paper'
-const scissors = 'scissors'
-let playerTurn;
-let computerTurn;
+function rockpaperScissors(playerTurn) {
+
+    const rock = 'rock'
+    const paper = 'paper'
+    const scissors = 'scissors'
+    let computerTurn;
 
 
-if (playerTurn === 'r' || playerTurn === 'rock') {
-    playerTurn = rock
-} else if (playerTurn === 'p' || playerTurn === 'paper') {
-    playerTurn = paper
-} else if (playerTurn === 's' || playerTurn === 'scissors') {
-    playerTurn = scissors
-} else {
-    console.log('Invalid input. Try again...')
+    if (playerTurn === 'r' || playerTurn === 'rock') {
+        playerTurn = rock
+    } else if (playerTurn === 'p' || playerTurn === 'paper') {
+        playerTurn = paper
+    } else if (playerTurn === 's' || playerTurn === 'scissors') {
+        playerTurn = scissors
+    } else {
+        console.log('Invalid input. Try again...')
+        return
+    }
+    console.log(`You choose ${playerTurn}`)
+
+    let computerRandomNumber = Math.floor(Math.random() * 3) + 1
+
+    if (computerRandomNumber === 1) {
+        computerTurn = rock
+    } else if (computerRandomNumber === 2) {
+        computerTurn = paper
+    } else if (computerRandomNumber === 3) {
+        computerTurn = scissors
+    }
+    console.log(`The computer choses ${computerTurn}`)
+
+    if ((playerTurn === 'rock' && computerTurn === 'rock') ||
+        (playerTurn === 'paper' && computerTurn === 'paper') ||
+        (playerTurn === 'scissors' && computerTurn === 'scissors')) {
+        console.log('this game was a draw!')
+    } else if ((playerTurn === 'rock' && computerTurn === 'scissors') ||
+        (playerTurn === 'paper' && computerTurn === 'rock') ||
+        (playerTurn === 'scissors' && computerTurn === 'paper')) {
+        console.log('You win!')
+    } else if ((playerTurn === 'rock' && computerTurn === 'paper') ||
+        (playerTurn === 'paper' && computerTurn === 'scissors') ||
+        (playerTurn === 'scissors' && computerTurn === 'rock')) {
+        console.log('You lose!')
+    }
+
 }
-console.log(`You choose ${playerTurn}`)
-
-let computerRandomNumber = Math.floor(Math.random() * 3) + 1
-
-if (computerRandomNumber === 1) {
-    computerTurn = rock
-} else if (computerRandomNumber === 2) {
-    computerTurn = paper
-} else if (computerRandomNumber === 3) {
-    computerTurn = scissors
-}
-console.log(`The computer choses ${computerTurn}`)
-
-if ((playerTurn === 'rock' && computerTurn === 'rock') ||
-    (playerTurn === 'paper' && computerTurn === 'paper') ||
-    (playerTurn === 'scissors' && computerTurn === 'scissors')) {
-    console.log('this game was a draw!')
-} else if ((playerTurn === 'rock' && computerTurn === 'scissors') ||
-    (playerTurn === 'paper' && computerTurn === 'rock') ||
-    (playerTurn === 'scissors' && computerTurn === 'paper')) {
-    console.log('You win!')
-} else if ((playerTurn === 'rock' && computerTurn === 'paper') ||
-    (playerTurn === 'paper' && computerTurn === 'scissors') ||
-    (playerTurn === 'scissors' && computerTurn === 'rock')) {
-    console.log('You lose!')
-}
+rockpaperScissors('w')

7a8927a741db344339a0a53eb4c9b6a9dc644d5d
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-09-25 12:15:39 +0300

    add feature to play until 3 wins or 3 losses

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
 const playerText = document.querySelector("#playerText");
 const computerText = document.querySelector("#computerText");
 const resultText = document.querySelector("#resultText");
+const finalText = document.querySelector("#finalText");
 const choiceBtns = document.querySelectorAll(".choiceBtn");
 
 let player;
 let computer;
+let wins = 0
+let losses = 0
 
 choiceBtns.forEach((button) => button.addEventListener("click", () => {
-        player = button.textContent;
-        computerTurn();
-        playerText.textContent = `Player: ${player}`;
-        computerText.textContent = `Computer: ${computer}`;
-        resultText.textContent = checkWinner();
-    })
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+    if (resultText.textContent === "You Win!") {
+        wins++
+    } else if (resultText.textContent === "You Lose!") {
+        losses++
+    }
+    finalText.textContent = finalWinner();
+})
 );
 
 function computerTurn() {
-    const randNum = Math.floor(Math.random() * 3) + 1;
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
 
-    if (randNum === 1) {
+    if (randomNumber === 1) {
         computer = "ROCK";
-    } else if (randNum === 2) {
+    } else if (randomNumber === 2) {
         computer = "PAPER";
-    } else if (randNum === 3) {
+    } else if (randomNumber === 3) {
         computer = "SCISSORS";
     }
 }
@@ -36,5 +45,13 @@ function checkWinner() {
         return player === "SCISSORS" ? "You Win!" : "You Lose!";
     } else if (computer == "SCISSORS") {
         return player == "ROCK" ? "You Win!" : "You Lose!";
+    }
+}
+
+function finalWinner() {
+    if (wins === 3) {
+        return `Final result is ${wins} - ${losses}. YOU HAVE WON THE GAME !!!`
+    } else if (losses === 3) {
+        return `Final result is ${wins} - ${losses}. YOU HAVE LOST THE GAME !!!`
     }
 }
